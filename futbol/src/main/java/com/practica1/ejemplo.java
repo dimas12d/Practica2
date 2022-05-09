c3d25138b2612b9083bcefab41c3361c057f91ba
diimas__ <72742989+dimas12d@users.noreply.github.com>
2022-05-09 09:57:45 +0200

    Add files via upload

---
+++ futbol/src/main/java/com/practica1/ejemplo.java
@@ -0,0 +1,52 @@
+package com.practica1;
+import org.xmldb.api.base.*;
+import org.xmldb.api.modules.*;
+import org.xmldb.api.*;
+import org.xmldb.api.modules.XMLResource;
+
+
+public class ejemplo {
+
+    private static String URI = "xmldb:exist://localhost:8080/exist/xmlrpc";
+
+    /**
+     * args[0] Should be the name of the collection to access
+     * args[1] Should be the XQuery to execute
+     */
+    public static void main(String args[]) throws Exception {
+
+        final String driver = "org.exist.xmldb.DatabaseImpl";
+
+        // initialize database driver
+        Class cl = Class.forName(driver);
+        Database database = (Database) cl.newInstance();
+        database.setProperty("create-database", "true");
+        DatabaseManager.registerDatabase(database);
+
+        Collection col = null;
+        try {
+            col = DatabaseManager.getCollection("xmldb:exist://localhost:8080/exist/xmlrpc/db/xml");
+            XQueryService xqs = (XQueryService) col.getService("XQueryService", "1.0");
+            xqs.setProperty("indent", "yes");
+
+            CompiledExpression compiled = xqs.compile("/");
+            ResourceSet result = xqs.execute(compiled);
+            ResourceIterator i = result.getIterator();
+            Resource res = null;
+            while(i.hasMoreResources()) {
+                try {
+                    res = i.nextResource();
+                    System.out.println(res.getContent());
+                } finally {
+                    //dont forget to cleanup resources
+                    //            try { ((EXistResource)res).freeResources(); } catch(XMLDBException xe) {xe.printStackTrace();}
+                }
+            }
+        } finally {
+            //dont forget to cleanup
+            if(col != null) {
+                try { col.close(); } catch(XMLDBException xe) {xe.printStackTrace();}
+            }
+        }
+    }
+}
